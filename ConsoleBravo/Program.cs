637c3242907ff41f53018273a4f1a505336463ef
Nikita Behmetev <nikitabehmetev@gmail.com>
2021-07-28 09:42:18 +0300

    Обновление слассов

--- ConsoleBravo/Program.cs
+++ ConsoleBravo/Program.cs
@@ -1,53 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Threading.Tasks;
+using HtmlAgilityPack;
 
-namespace ConsoleBravo
+namespace brav_o.ru
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            string articul = System.IO.File.ReadAllText(@"articul.txt", Encoding.Default);//Копируем нажные артикулы в фаил 
-
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            string articul = System.IO.File.ReadAllText(@"articul.txt", Encoding.Default);//Копируем нужные артикулы из фаила 
+                                                                                          // доработать - в место строки использовать массив
+            articul = articul.Replace("Р‘", "Б");// костыльно убираем Б
+            articul = articul.Replace("Б", "");// костыльно убираем Б
             articul = articul.Replace(" ", "");//убираем пробелы
             articul = articul.Replace("\r\n", ","); //переносы строк - заменяем на символ
-
+            Console.WriteLine(articul);
 
             String value = articul;
             Char delimiter = ',';
 
             String[] substrings = value.Split(delimiter);
+            //Console.WriteLine(substrings.Length);
+
+            string Filepath = @"Text.csv";
+
+            var wc = new WebClient();
+
             String[] fibarray = value.Split(delimiter);
+
             foreach (string element in fibarray)
             {
+                var html = @"https://brav-o.ru/catalog/?q=" + element;
+                HtmlWeb web = new HtmlWeb();
+                var htmlDoc = web.Load(html);
 
-                FtpWebRequest frpWebRequest = (FtpWebRequest)FtpWebRequest.Create("ftp://85.235.48.6:5251/НОВИНКИ/" + element + ".jpg");
-                frpWebRequest.Credentials = new NetworkCredential("bravo_guest", "Veseluha");
-                frpWebRequest.KeepAlive = true;
-                frpWebRequest.UsePassive = true;
-                frpWebRequest.UseBinary = true;
-                frpWebRequest.Method = WebRequestMethods.Ftp.DownloadFile;
-                FtpWebResponse response = (FtpWebResponse)frpWebRequest.GetResponse();
-                Stream stream = response.GetResponseStream();
-                List<byte> list = new List<byte>();
-                int b;
-                while ((b = stream.ReadByte()) != -1)
-                    list.Add((byte)b);
-                File.WriteAllBytes("img/Б"+ element + ".jpg", list.ToArray());
+                    try
+                    {
+                    //*[@id="bx_3966226736_17220_362ce596257894d11ab5c1d73d13c755"]/div/div[2]/div[1]/div[1]
+                    foreach (HtmlNode node2 in htmlDoc.DocumentNode.SelectNodes("//*[@class='items-row']/div[1]/div[1]/div[1]/div[1]/a[1]"))
+                        {
 
-                Console.WriteLine("Картинка " + element + ".jpg скачана");
+                            string value2 = node2.OuterHtml;
 
-                stream.Close();
-                response.Close();
-            }
-            Console.WriteLine("press any key to continue... ");
-            Console.ReadLine();
+
+                            string[] mystring = value2.Split('"');
+
+                            Console.WriteLine(mystring[1]);
+                            var html2 = @"https://brav-o.ru" + mystring[1];
+                            HtmlWeb web2 = new HtmlWeb();
+                            var htmlDoc2 = web.Load(html2);
+
+                            foreach (HtmlNode node3 in htmlDoc2.DocumentNode.SelectNodes("//*[@class='desc-box__txt']"))
+                            {
+                               
+
+                                string testTar = node3.InnerText;
+
+                                testTar = testTar.Replace("&#37;", "%"); //переносы строк - заменяем на символ
+                                testTar = testTar.Replace("&quot;", "\""); //переносы строк - заменяем на символ
+                                testTar = testTar.Replace("&nbsp;", " "); //переносы строк - заменяем на символ
+                                testTar = testTar.Replace("\n", " "); //переносы строк - заменяем на символ
+                                testTar = testTar.Replace("\r", " "); //переносы строк - заменяем на символ
+                                testTar = testTar.Trim();
+
+
+                            Console.WriteLine(element);
+                                Console.WriteLine(testTar);
+
+                                if (node3.InnerText != "")
+                                {
+                                    //File.AppendAllText(Filepath, node3.InnerText + '\n', Encoding.UTF8);
+
+                                    string first = "\"" + element + "\"";
+                                    string second = testTar;
+                                    string csv = string.Format("{0} ; {1}\n", first, second);
+                                    File.AppendAllText(Filepath, csv);
+
+                                }
+                                else
+                                {
+                                    string first = element;
+                                    string second = " ";
+                                    string csv = string.Format("{0} ; {1}\n", first, second);
+                                    File.AppendAllText(Filepath, csv);
+                                }
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        string first = "\"" + element + "\"";
+                        string second = "";
+                        string csv = string.Format("{0} ; {1}\n", first, second);
+                        File.AppendAllText(Filepath, csv);
+                    }
+                }
+            System.Console.WriteLine();
+            Console.Read();
         }
     }
 }
